50b4353fc58f950eb547d42720b4eb1d972e7d45
Joshua Acosta <Acosta.joshua03@gmail.com>
2019-08-02 15:42:29 -0400

    update api design
    -update/create resources and methods for getting or updating user balance and getting and and posting transactions from/to the database.
    -used flask-restful fields to format get responses.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,9 +1,11 @@
+"""API implementation """
 from flask import Blueprint
 from flask_restful  import Api
-from .resources import BalanceResources
+from .resources import BalanceResources, TransactionResources
 
 api_bp = Blueprint('api', __name__)
 
 api = Api(api_bp)
 
-api.add_resource(BalanceResources, '/api/balance/me')
+api.add_resource(BalanceResources, '/api/balance/me', methods=["GET", "PUT"])
+api.add_resource(TransactionResources, '/api/transaction/me', methods=["GET", "POST"])
--- app/api/resources.py
+++ app/api/resources.py
@@ -1,13 +1,72 @@
-from flask import jsonify
-from flask_restful import Resource
-from app.models import Balance
+"""API """
+from flask_restful import Resource, fields, marshal_with
 from flask_login import current_user
+from flask import request
+from app.models import Balance, Transaction
+from app import db
+
+
+get_balance_fields = {
+    'user_id': fields.Integer,
+    'balance': fields.Float
+}
+
+get_transaction_fields = {
+    "transaction_id": fields.Integer,
+    "stock_symbol": fields.String,
+    "quantity": fields.Integer,
+    "date_purchased": fields.DateTime,
+    "user_id": fields.Integer
+}
 
 
 class BalanceResources(Resource):
+    """HTTP API methods regarding current user's balance"""
+    @marshal_with(get_balance_fields)
     def get(self):
+        """Returns current balance for user"""
         if current_user.is_authenticated:
             id_num = current_user.get_id()
-            current_user_balance = Balance.query.filter_by(user_id=id_num).first()
-            return jsonify(float(current_user_balance.balance))
-        
+            return  Balance.query.filter_by(user_id=id_num).first()
+
+    def put(self):
+        """Updates current user's balance"""
+        if (current_user.is_authenticated) and (request.method == 'PUT'):
+            data = request.get_json()
+            new_balance = data["new_balance"]
+            id_num = current_user.get_id()
+            user_new_balance = Balance.query.filter_by(user_id=id_num).first()
+            user_new_balance.balance = new_balance
+            db.session.commit()
+            return {"result":"success"}
+
+
+class TransactionResources(Resource):
+    """HTTP API methods regarding current user's transactions"""
+    @marshal_with(get_transaction_fields)
+    def get(self):
+        """Returns all transactions for user"""
+        if current_user.is_authenticated:
+            id_num = current_user.get_id()
+            return Transaction.query.filter_by(user_id=id_num).all()
+
+    def post(self):
+        """Post new transaction completed into db, returns successful status"""
+        if (current_user.is_authenticated) and (request.method == 'POST'):
+            data = request.get_json()
+
+            stock_symbol = data["stock_symbol"]
+            quantity = data["quantity"]
+            date_purchased = data["date_purchased"]
+            user_id = current_user.get_id()
+
+            new_transactions = Transaction(
+                stock_symbol=stock_symbol,
+                quantity=quantity,
+                date_purchased=date_purchased,
+                user_id=user_id
+                )
+            db.session.add(new_transactions)
+            db.session.commit()
+
+            return {"result":"success"}
